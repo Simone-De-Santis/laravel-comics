d809e978f076d67242a578c9544ffb534e7526aa
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-02 12:57:48 +0200

    modifica scss e html header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,32 @@
 
 <body>
     <header>
+        <div class="top-header">
+            <div class="container">
+                <span>DC POWER VISA &#8480 </span>
+                <span>ADDITIONAL DC SITE &#9660 </span>
+            </div>
+        </div>
+        <div class="container nav-bar">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+            <div class="link-nav">
+
+                <ul>
+                    <li><a href="#" class="active"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                    <li><a href="#"> ciao ciao </a></li>
+                </ul>
+                <input type="text" placeholder="search &#128269">
+            </div>
+
+        </div>
 
 
     </header>
