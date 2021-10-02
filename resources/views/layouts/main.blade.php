09450944f3b5167c41ef69e8fd2e54b9f3e826bd
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-02 19:19:42 +0200

    creata icon bar - footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,24 +2,29 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Home DC | @yield('title')</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <title>Home DC | @yield('title')</title>
 </head>
 
 <body>
-    @include('includes.header')
-    <main>
-        <section class="container-hero">
-        </section>
-     @yield('content')        
-    </main>
+  {{-- 'componente header' --}}
+  @include('includes.header')
+  {{-- sezione main HERO --}}
+  <main>
+    <section class="container-hero">
+    </section>
+    @yield('content')
+  </main>
+  {{-- 'componente footer' --}}
+  @include('includes.footer')
 
 
 
-    <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 
 </html>
