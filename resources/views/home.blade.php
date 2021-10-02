7fe3af5ebeff56b2bc6fda5b15fcf2a69a8fc59e
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-02 13:35:41 +0200

    gestito section hero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home DC</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Home DC</title>
 </head>
 
 <body>
@@ -18,9 +18,8 @@
             </div>
         </div>
         <div class="container nav-bar">
-            <img src="{{ asset('images/dc-logo.png') }}" alt="logo">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="logo" class="logo">
             <div class="link-nav">
-
                 <ul>
                     <li><a href="#" class="active"> ciao ciao </a></li>
                     <li><a href="#"> ciao ciao </a></li>
@@ -35,11 +34,11 @@
                 </ul>
                 <input type="text" placeholder="search &#128269">
             </div>
-
         </div>
-
-
     </header>
+    <section class="container-hero">
+        
+    </section>
 
 
 
